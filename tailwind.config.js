c5f54b9732e579725608c4ad77bcc0e02fbf76d7
ebere <ebereuzodufa@gmail.com>
2023-03-18 11:32:28 +0100

    chore: Scaffolds app

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,25 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      green: {
+        DEFAULT: "#4daa96",
+      },
+      black: {
+        DEFAULT: "#060607",
+      },
+      gray: {
+        DEFAULT: "#2a2d40",
+      },
+      pink: {
+        DEFAULT: "#E55986",
+      },
+      white: {
+        DEFAULT: "#000",
+        smoke: "#FEFEFE",
+      },
+    },
   },
   plugins: [],
 };
