fd420590a752cd249dbab9fdf34692acdde673ac
ebere <ebereuzodufa@gmail.com>
2023-03-18 15:37:34 +0100

    feat: public mobile footer menu

--- src/app/features/public/public.module.ts
+++ src/app/features/public/public.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
 import { HomeComponent } from './pages/home/home.component';
-
+import { NavbarComponent } from './layout/components/navbar/navbar.component';
+import { FooterMenuComponent } from './layout/components/footer-menu/footer-menu.component';
+import { LayoutComponent } from './layout/layout.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    FooterMenuComponent,
+    LayoutComponent,
   ],
-  imports: [
-    CommonModule,
-    PublicRoutingModule
-  ]
+  imports: [CommonModule, PublicRoutingModule, SharedModule],
 })
-export class PublicModule { }
+export class PublicModule {}
