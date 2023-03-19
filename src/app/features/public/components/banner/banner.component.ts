12297ddacf0856618aa299b79506d72124403595
ebere <ebereuzodufa@gmail.com>
2023-03-20 00:06:03 +0100

    ref: home page styles based icon changes

--- src/app/features/public/components/banner/banner.component.ts
+++ src/app/features/public/components/banner/banner.component.ts
@@ -7,25 +7,37 @@ import { BANNER_LIST, CATEGORY_LIST } from 'src/app/core/constants/home';
   styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent implements OnInit {
+  /**]
+   * @param swipeDirection is 1 for left & -1 for right
+   */
+  private swipeDirection = 1;
+
   bannerList = BANNER_LIST;
   selectSlideIndex = 0;
-  diff = 1;
   categoryList = CATEGORY_LIST;
   constructor() {}
 
   ngOnInit(): void {}
 
-  selectSlide(index: number) {
-    if (index + 1 > 2) {
-      this.diff = -1;
-    } else if (index === 0) {
-      this.diff = 1;
-    }
-
-    this.selectSlideIndex = index + this.diff;
-  }
-
   trackByFn(index: number) {
     return index;
+  }
+
+  swipeLeft(event: Event) {
+    if (this.selectSlideIndex + 1 < this.bannerList.length) {
+      this.swipeDirection = 1;
+      this.swipeSlide();
+    }
+  }
+
+  swipeRight(event: Event) {
+    if (this.selectSlideIndex > 0) {
+      this.swipeDirection = -1;
+      this.swipeSlide();
+    }
+  }
+
+  private swipeSlide() {
+    this.selectSlideIndex = this.selectSlideIndex + this.swipeDirection;
   }
 }
