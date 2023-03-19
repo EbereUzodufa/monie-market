ed8e71e32dc1903edc45f1272e3e9f290468761c
ebere <ebereuzodufa@gmail.com>
2023-03-20 00:03:00 +0100

    feat: implements directive for display masked text as in UI

---
+++ src/app/shared/directives/suptext-masked-characters.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SuptextMaskedCharactersDirective } from './suptext-masked-characters.directive';
+
+describe('SuptextMaskedCharactersDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SuptextMaskedCharactersDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,8 @@ import { SdkIconComponent } from './components/sdk-icon/sdk-icon.component';
 import { SdkBadgeComponent } from './components/sdk-badge/sdk-badge.component';
 import { ProductCardComponent } from './components/products/product-card/product-card.component';
 import { ProductListComponent } from './components/products/product-list/product-list.component';
+import { SuptextMaskedCharactersDirective } from './directives/suptext-masked-characters.directive';
+import { ProductRatingComponent } from './components/products/product-rating/product-rating.component';
 
 @NgModule({
   declarations: [
@@ -12,8 +14,10 @@ import { ProductListComponent } from './components/products/product-list/product
     SdkBadgeComponent,
     ProductCardComponent,
     ProductListComponent,
+    SuptextMaskedCharactersDirective,
+    ProductRatingComponent,
   ],
   imports: [CommonModule, RouterModule],
-  exports: [SdkIconComponent, SdkBadgeComponent, ProductListComponent],
+  exports: [SdkIconComponent, SdkBadgeComponent, ProductListComponent, SuptextMaskedCharactersDirective, ProductRatingComponent],
 })
 export class SharedModule {}
